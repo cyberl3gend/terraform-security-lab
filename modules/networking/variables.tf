a0c65b9924359cafd37e020258610e725d9f5967
Jonathan <jcrowder4456@gmail.com>
2026-09-09 19:42:54 -0400

    fix: auto-format terraform files

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -10,7 +10,7 @@ variable "allowed_ingress_ports" {
 }
 
 variable "vpc_cidr" {
-  type = string
+  type        = string
   description = "CIDR block for the VPC"
-  default = "10.0.0.0/16"
+  default     = "10.0.0.0/16"
 }
