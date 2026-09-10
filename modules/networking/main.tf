c1f2fc954f8af8611b9b7d797efe9e4053779fbd
Jonathan <jcrowder4456@gmail.com>
2026-09-09 21:34:32 -0400

    fix: add default sg lockdown and vpc flow logs

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -39,3 +39,55 @@ resource "aws_security_group_rule" "allow_egress" {
   security_group_id = aws_security_group.app_sg.id
   description       = "Allow outbound HTTPS for updates"
 }
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.environment}-default-sg"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/${var.environment}-flow-logs"
+  retention_in_days = 90
+}
+
+resource "aws_iam_role" "flow_logs" {
+  name = "${var.environment}-vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "vpc-flow-logs.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_logs" {
+  name = "${var.environment}-vpc-flow-logs-policy"
+  role = aws_iam_role.flow_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_flow_log" "this" {
+  vpc_id          = aws_vpc.this.id
+  traffic_type    = "ALL"
+  iam_role_arn    = aws_iam_role.flow_logs.arn
+  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
+}
